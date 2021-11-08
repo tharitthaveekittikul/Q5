e4fa0883263dc3fbf412c082c167210486d3e85e
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2021-11-08 11:54:05 +0700

    update tree(2)

--- directorySearch.java
+++ directorySearch.java
@@ -10,37 +10,14 @@ import java.text.SimpleDateFormat;
 public class directorySearch {
     public static String path;
     public static String xmlString = "";
+    public static int count = 0;
     public static void main(String[] args) {
         path = args[0];
-        findPath(path);
-        findPath2(path);
+        findPath(path,count);
 //        System.out.println(xmlString);
     }
-    public static void findPath(String path){
-        String[] listpath = path.split("/");
-        String lastpath = listpath[listpath.length-1];
-        xmlString += "<folder name=" + '"' + lastpath + '"' + ">";
-        File f = new File(path);
-        File[] files =  f. listFiles();
-//        System.out.println(path);
-        for ( int i = 0 ; i < files.length ; i++ ){
-            File file = files[i];
-//            System.out.println(file);
-            String filename = file.getName();
-//            System.out.println(filename);
-            if ( file.isDirectory() ){
-                String nextpath = files[i].toString();
-//                System.out.println(nextpath);
-                findPath(nextpath);
-            }
-            else {
-                xmlString += "<file>" + filename + "</file>";
-            }
-        }
-        xmlString += "</folder>";
-    }
 
-    public static void findPath2(String path){
+    public static void findPath(String path,int count){
         String[] listpath = path.split("/");
         String lastpath = listpath[listpath.length-1];
         xmlString += "<folder name=" + '"' + lastpath + '"' + ">";
@@ -52,16 +29,44 @@ public class directorySearch {
 //            System.out.println(file);
             String filename = file.getName();
 //            System.out.println(filename);
-            if ( file.isDirectory() ){
-                String nextpath = files[i].toString();
-//                System.out.println(nextpath);
-                System.out.print("\t|_________");
-                findPath2(nextpath);
+
+            if ( count > 0 ){
+                for (int j = 0 ; j < count ; j++){
+                    if ( count > 1 ){
+                        for (int k = 0 ; k < count ; k++){
+                            System.out.print("  ");
+                        }
+                    }
+                    System.out.println("\t"+"|_________"+filename);
+                }
             }
-            else {
-                System.out.println("\t\t"+filename);
-                xmlString += "<file>" + filename + "</file>";
+
+            if (files[i].isDirectory()){
+//                System.out.println("\t"+"|_________"+files[i]);
+                count += 1;
+                path = files[i].toString();
+//                println("Directory: " + path);
+                findPath(path,count);
+                count = 0;
             }
+//            if ( file.isDirectory() ){
+//                String nextpath = files[i].toString();
+////                System.out.println(nextpath);
+//                count++;
+//                System.out.print("\t|_________");
+//                findPath(nextpath);
+//            }
+//            else {
+//                for( int j = 0 ; j < count ; j++) {
+//                    if( count > 1){
+//                        for (int k = 0; k < count ; k++){
+//                            System.out.print(" ");
+//                        }
+//                    }
+//                }
+//                System.out.println("\t"+"|_________"+filename);
+//                xmlString += "<file>" + filename + "</file>";
+//            }
         }
         xmlString += "</folder>";
     }
