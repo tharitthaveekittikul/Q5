05cd8080ca8aa684197ae473d6b7df89533f1f57
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2021-11-08 11:19:22 +0700

    first commit

---
+++ directorySearch.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+
+public class directorySearch {
+    public static String path;
+    public static String xmlString = "";
+    public static void main(String[] args) {
+        path = args[0];
+        findPath(path);
+        System.out.println(xmlString);
+    }
+    public static void findPath(String path){
+        String[] listpath = path.split("/");
+        String lastpath = listpath[listpath.length-1];
+        xmlString += "<folder name=" + '"' + lastpath + '"' + ">";
+        File f = new File(path);
+        File[] files =  f. listFiles();
+//        System.out.println(path);
+        for ( int i = 0 ; i < files.length ; i++ ){
+            File file = files[i];
+//            System.out.println(file);
+            String filename = file.getName();
+//            System.out.println(filename);
+            if ( file.isDirectory() ){
+                String nextpath = files[i].toString();
+//                System.out.println(nextpath);
+                findPath(nextpath);
+            }
+            else {
+                xmlString += "<file>" + filename + "</file>";
+            }
+        }
+        xmlString += "</folder>";
+    }
+}
