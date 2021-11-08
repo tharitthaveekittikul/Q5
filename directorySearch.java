85a0f0003846c5a916fae4b58c0a33ff33b86b06
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2021-11-08 11:27:06 +0700

    update tree

--- directorySearch.java
+++ directorySearch.java
@@ -13,7 +13,8 @@ public class directorySearch {
     public static void main(String[] args) {
         path = args[0];
         findPath(path);
-        System.out.println(xmlString);
+        findPath2(path);
+//        System.out.println(xmlString);
     }
     public static void findPath(String path){
         String[] listpath = path.split("/");
@@ -33,6 +34,32 @@ public class directorySearch {
                 findPath(nextpath);
             }
             else {
+                xmlString += "<file>" + filename + "</file>";
+            }
+        }
+        xmlString += "</folder>";
+    }
+
+    public static void findPath2(String path){
+        String[] listpath = path.split("/");
+        String lastpath = listpath[listpath.length-1];
+        xmlString += "<folder name=" + '"' + lastpath + '"' + ">";
+        File f = new File(path);
+        File[] files =  f. listFiles();
+        System.out.println(lastpath);
+        for ( int i = 0 ; i < files.length ; i++ ){
+            File file = files[i];
+//            System.out.println(file);
+            String filename = file.getName();
+//            System.out.println(filename);
+            if ( file.isDirectory() ){
+                String nextpath = files[i].toString();
+//                System.out.println(nextpath);
+                System.out.print("\t|_________");
+                findPath2(nextpath);
+            }
+            else {
+                System.out.println("\t\t"+filename);
                 xmlString += "<file>" + filename + "</file>";
             }
         }
